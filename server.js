636183104777012a3108defef25b545edc8f7877
joeaa223 <jiongzhengwu@gmail.com>
2025-06-09 12:37:10 +0800

    delete

--- server.js
+++ server.js
@@ -2,17 +2,6 @@ const http = require('http');
 
 const app = require('./backend/app');
 
-// Create HTTP server
 const server = http.createServer(app);
 
-// Check if running on Vercel
-if (process.env.VERCEL) {
-  // Export the server for Vercel
-  module.exports = app;
-} else {
-  // Local development
-  const PORT = process.env.PORT || 3000;
-  server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-}
+server.listen(3000);

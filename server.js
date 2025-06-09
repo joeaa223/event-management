1df3e49dec215643a1c6948d3cb37c0446c2fec0
joeaa223 <jiongzhengwu@gmail.com>
2025-06-09 12:57:58 +0800

    update backend

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const http = require('http');
 
 const app = require('./backend/app');
 
+const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
-server.listen(3000);
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
